c4f8d112e26e260494f8902d740af7a936eee290
honeniq <honeniq@gmail.com>
2014-12-02 07:25:00 +0900

    Gemfile追加

--- led_blink/led_blink.rb
+++ led_blink/led_blink.rb
@@ -1,19 +1,13 @@
+require 'pi_piper'
+require 'pry' 
 
-exp = open("/sys/class/gpio/export", "w")
-exp.write(24)
-exp.close
+pin = PiPiper::Pin.new(:pin => 24, :direction => :out)
  
-dir = open("/sys/class/gpio/gpio24/direction", "w")
-dir.write("out")
-dir.close
- 
-out = 1
-50.times do
-  red = open("/sys/class/gpio/gpio24/value", "w")
-  red.write(out)
-  red.close
-  out = out == 1 ? 0 : 1
-  sleep 0.1
+10.times do
+  pin.on
+  sleep 0.5
+  pin.off
+  sleep 0.5
 end
  
 uexp = open("/sys/class/gpio/unexport", "w")
